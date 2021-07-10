a63f8b74d05691fb0a573a7f6c65031144dd22d3
Marcus Johansson <majohansson@gmail.com>
2021-07-10 04:04:35 +0200

    added support for multiple lights (#11)

--- elgato.dart
+++ elgato.dart
@@ -1,15 +1,16 @@
 import 'dart:async';
 import 'dart:convert' as convert;
 import 'dart:io';
+
 import 'package:dcli/dcli.dart';
 import 'package:http/http.dart' as http;
 import 'package:multicast_dns/multicast_dns.dart';
 
-// We store the results of the IP lookup in a file for future speedups
+// Store the results of the IP lookup locally to speed things up
 final cacheFile = join(HOME, ".elgato.dart.cache");
 
-String lightIpPort = "";
-Uri url = Uri.parse("http://$lightIpPort/elgato/lights");
+var lightIpPorts = [];
+var urls = lightIpPorts.map((x) => Uri.parse('http://$x/elgato/lights'));
 
 /// Discover Elgato lights on the network
 findLights() async {
@@ -17,65 +18,70 @@ findLights() async {
   final MDnsClient client = MDnsClient();
   await client.start();
 
-  await for (PtrResourceRecord ptr in client
-      .lookup<PtrResourceRecord>(ResourceRecordQuery.serverPointer(svcName))) {
-    await for (SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
-        ResourceRecordQuery.service(ptr.domainName))) {
-      await for (IPAddressResourceRecord ip
-          in client.lookup<IPAddressResourceRecord>(
-              ResourceRecordQuery.addressIPv4(srv.target))) {
-        lightIpPort = '${ip.address.host}:${srv.port}';
+  var svcPointer = ResourceRecordQuery.serverPointer(svcName);
+  await for (var ptr in client.lookup<PtrResourceRecord>(svcPointer)) {
+    var svcQuery = ResourceRecordQuery.service(ptr.domainName);
+    await for (var srv in client.lookup<SrvResourceRecord>(svcQuery)) {
+      var svcName = ResourceRecordQuery.addressIPv4(srv.target);
+      await for (var ip in client.lookup<IPAddressResourceRecord>(svcName)) {
+        lightIpPorts.add('${ip.address.host}:${srv.port}');
       }
     }
   }
   client.stop();
 }
 
-// Flips zeroes and ones
+// Flip zeroes and ones
 flipInt(inputInt) {
   return inputInt = 1 - inputInt;
 }
 
-/// Flips the lights
-flipSwitch() async {
+/// Switch a light
+Future<bool> flipSwitch(var url) async {
   var rawResponse;
 
   try {
-    // Await the http get response, then decode the json-formatted response.
+    // Await the HTTP GET response, then decode the JSON-formatted response
     rawResponse = await http.get(url).timeout(const Duration(seconds: 3));
   } on TimeoutException {
-    // If we didn't get a response, bail.
-    print('timed out calling lights, exiting...');
-    exit(2);
+    print('Timed out calling lights, exiting...');
+    return false;
   } on SocketException catch (e) {
     print("Failed to connect to light, error:\n${e}");
-    exit(2);
+    return false;
   }
-
   var currentState = convert.jsonDecode(rawResponse.body);
 
-  // Flip every light's `on` value
+  // Flip the light's `on` value
   currentState['lights'].forEach((light) => light['on'] = flipInt(light['on']));
 
-  // Now PUT the `currentState` back to the device
+  // Update the light with the newly flipped value
   await http.put(url, body: convert.jsonEncode(currentState));
+
+  return true;
 }
 
 void main() async {
   if (exists(cacheFile)) {
-    read(cacheFile).forEach((line) => lightIpPort = line);
+    read(cacheFile).forEach((line) => lightIpPorts.add(line));
   } else {
     await findLights();
     if (cacheFile.isNotEmpty) {
-      cacheFile.write(lightIpPort);
-      print("wrote ${lightIpPort} to ${cacheFile}.");
+      cacheFile.write(lightIpPorts.join('\n'));
+      print("Wrote ${lightIpPorts.join(', ')} to ${cacheFile}.");
     }
   }
 
-  if (lightIpPort.isEmpty) {
-    print("no lights found in ${cacheFile} file, exiting...");
+  if (lightIpPorts.isEmpty) {
+    print("No lights found, exiting...");
     exit(2);
   }
 
-  flipSwitch();
+  // Execute flipSwitch() for all URLs in parallell
+  Future.wait(urls.map((url) => flipSwitch(url))).then((List<bool> retValues) {
+    // Return exit code 2 if any of the lights failed to flip
+    if (retValues.any((x) => x == false)) {
+      exit(2);
+    }
+  });
 }
